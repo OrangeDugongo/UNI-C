c035fe3ca4171ab8e537a920f24fbef1d16bf383
OrangeDugongo <raffy.m.96@gmail.com>
2016-03-10 19:35:32 +0100

    delete print

--- Vettori e Puntatori/Matrici/Stringa.c
+++ Vettori e Puntatori/Matrici/Stringa.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #define MAXLINE 10000
 #define MAX 4
-void Print(int);
 char str[MAX][MAXLINE];
 
 int main(){
@@ -14,16 +13,10 @@ int main(){
                 space[j]++;
             str[j][i]=c;
         }
+        str[j][i]="\0";
     }
     for(j=0;j<MAX;j++){
-        Print(j);
-        printf(": spazi %d, parole %d\n", space[j], space[j]+1);
+        printf("%s: spazi %d, parole %d\n", str[j], space[j], space[j]+1);
     }
 
-}
-
-void Print(int j){
-    int i;
-    for(i=0;i<MAXLINE && str[i]!='\n';i++)
-        printf("%c", str[j][i]);
 }
