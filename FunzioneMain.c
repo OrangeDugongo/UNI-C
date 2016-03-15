100f545a8e87bcbd306c5ff0b5d7abe4bb840950
OrangeDugongo <raffy.m.96@gmail.com>
2016-03-15 19:39:43 +0100

    parametri funzione main

---
+++ FunzioneMain.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+  int sum=0, i;
+  for(i=1;i<argc;i++)
+    sum+=atoi(argv[i]);
+  printf("la somma è: %d", sum);
+  return 0;
+}
