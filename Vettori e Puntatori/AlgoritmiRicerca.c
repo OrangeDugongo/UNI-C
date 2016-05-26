d2e65352cf15bc40613ca9bb749a3a4209bbc48f
OrangeDugongo <raffy.m.96@gmail.com>
2016-05-26 15:27:10 +0200

    Dicotomica ricorsiva

--- Vettori e Puntatori/AlgoritmiRicerca.c
+++ Vettori e Puntatori/AlgoritmiRicerca.c
@@ -2,6 +2,7 @@
 int RicercaVettoreNonOrdinato(int lista[], int n, int x, int *count);
 int RicercaVettoreOrdinato(int lista[], int n, int x, int *count);
 int RicercaBinaria(int lista[], int n, int x, int *count);
+int DicotomicaRicorsiva(int a[], int x, int eINF, int eSUP);
 
 int main(){
   int c, lista[10]={1,3,5,6,7,8,10,13,14,15}, count=0;//count serve per vedere quante volte viene eseguito il ciclo
@@ -52,4 +53,16 @@ int RicercaBinaria(int lista[], int n, int x, int *count){
       k = p - 1;
   }
   return(-1);
+}
+
+int DicotomicaRicorsiva(int a[], int x, int eINF, int eSUP){
+  int pm=(eSUP+eINF)/2;
+  if(eSUP<=eINF)
+    return -1;
+  else if(a[pm]==x)
+    return pm;
+  else if(a[pm]>x)
+    return DicotomicaRicorsiva(a, x, eINF, pm-1);
+  else
+    return DicotomicaRicorsiva(a, x, pm+1, eSUP);
 }
